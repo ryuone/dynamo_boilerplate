c80a06962ea8dff967a3f5b7a344fedc53149ea4
Ryuichi Maeno <ryuone@gmail.com>
2013-08-12 09:29:01 +0900

    Init

---
+++ mix.exs
@@ -0,0 +1,24 @@
+defmodule DynamoBoilerplate.Mixfile do
+  use Mix.Project
+
+  def project do
+    [ app: :dynamo_boilerplate,
+      version: "0.0.1",
+      dynamos: [DynamoBoilerplate.Dynamo],
+      compilers: [:elixir, :dynamo, :app],
+      env: [prod: [compile_path: "ebin"]],
+      compile_path: "tmp/#{Mix.env}/dynamo_boilerplate/ebin",
+      deps: deps ]
+  end
+
+  # Configuration for the OTP application
+  def application do
+    [ applications: [:cowboy, :dynamo],
+      mod: { DynamoBoilerplate, [] } ]
+  end
+
+  defp deps do
+    [ { :cowboy, github: "extend/cowboy" },
+      { :dynamo, "0.1.0-dev", github: "elixir-lang/dynamo" } ]
+  end
+end
