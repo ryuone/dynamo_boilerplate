75920ae1cd1632e8f37f8b8c243498b93ad1780d
Ryuichi Maeno <ryuone@gmail.com>
2013-08-17 14:56:00 +0900

    Add doctest.

--- lib/dynamo_boilerplate/eredis_pool.ex
+++ lib/dynamo_boilerplate/eredis_pool.ex
@@ -1,13 +1,21 @@
 defmodule DynamoBoilerplate.EredisPool do
   require Lager, as: Log
 
+  @doc """
+  Return a binary (string) version of our parameter.
+
+  ## Examples
+
+      iex> DynamoBoilerplate.EredisPool.set_strings_data('name', 'ryuone')
+      {:ok, "OK"}
+      iex> DynamoBoilerplate.EredisPool.get_strings_data('name')
+      {:ok, "ryuone"}
+  """
   def set_strings_data(key, value) when is_list(key) and is_list(value) do
     :eredis_pool.q({:global, :default}, ['set', key, value])
   end
 
   def get_strings_data(key) when is_list(key) do
-    r = :eredis_pool.q({:global, :default}, ['get', key])
-    Log.info("get_strings_data result : ~p", [r])
-    r
+    :eredis_pool.q({:global, :default}, ['get', key])
   end
 end
